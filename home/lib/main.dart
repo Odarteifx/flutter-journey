78f215b8b8bc87ed309bdf361542f8e409a42c48
william lamptey <williamlamp512@gmail.com>
2023-10-21 20:53:23 +0000

    Hello Flutter

---
+++ home/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const FTFLUTTER());
+
+
+class FTFLUTTER extends StatelessWidget {
+  const FTFLUTTER({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My First Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello World!'),
+        ) ,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
