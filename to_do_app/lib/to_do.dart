02f0fc8b18950d5410732f0dff3ed298e6ce1026
william lamptey <williamlamp512@gmail.com>
2024-05-17 23:53:34 +0000

    added first row

--- to_do_app/lib/to_do.dart
+++ to_do_app/lib/to_do.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:iconsax/iconsax.dart';
+
 
 class MyToDoInterface extends StatefulWidget {
   const MyToDoInterface({super.key});
@@ -12,27 +13,47 @@ class _MyToDoInterfaceState extends State<MyToDoInterface> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: SafeArea(
+      bottomNavigationBar: BottomNavigationBar(items: const [
+         BottomNavigationBarItem(icon: Icon(Iconsax.home), label: ''),
+         BottomNavigationBarItem(icon: Icon(Iconsax.tick_circle), label: ''),
+      ]),
+      body:  SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
+          padding:  const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const CircleAvatar(
+                   const CircleAvatar(
                     radius: 30,
+                    child: Text('O'),
                   ),
-                  const Text('Hi Odartei'),
+                   const Text(
+                    'Hi Odartei',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                    ),
                   Container(
                     decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(50)
+                       color: Colors.amber,
+                      borderRadius: BorderRadius.circular(100),
+                      
+
                     ),
-                    
+                   
                     width: 60,
                     height: 60,
-                  )
+                     child: const Center(
+                       child:  Badge(
+                        child: Icon(
+                          Iconsax.notification
+                          )
+                          ),
+                     ),
+                   )
                 ],
               )
             ],
